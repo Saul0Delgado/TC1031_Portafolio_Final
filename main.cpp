d6ec03dd31989cb5e350fe0cf46acc4bdfaec4c1
Hans-a01252926 <a01252926@tec.mx>
2023-10-14 21:57:18 -0700

    Primer Intento 2.3
    Agrega funcionalidad IP

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ struct Registro {
     string mensaje;
 };
 
-bool compareRegistros(const Registro& a, const Registro& b) {
+bool compareRegistrosDate(const Registro& a, const Registro& b) {
     // Orden de los meses
     unordered_map<std::string, int> months = {
         {"Jan", 1}, {"Feb", 2}, {"Mar", 3}, {"Apr", 4},
@@ -36,8 +36,20 @@ bool compareRegistros(const Registro& a, const Registro& b) {
     return months[a.fecha] < months[b.fecha] || (months[a.fecha] == months[b.fecha] && a.dia < b.dia) || (months[a.fecha] == months[b.fecha] && a.dia == b.dia && a.hora < b.hora) ;
 }
 
+bool compareRegistrosIp(const Registro& a, const Registro& b) {
+    string ipA = a.ip;
+    string ipB = b.ip;
+    
+    if (ipA != ipB) {
+        return ipA < ipB;
+    }
+
+    return ipA < ipB;
+}
+
 void read(vector<Registro>& registros);
-void searchAndDisplay(const vector<Registro>& registros);
+void searchAndDisplayDate(const vector<Registro>& registros);
+void searchAndDisplayIp(const vector<Registro>& registros);
 void saveSortedData(const vector<Registro>& registros);
 
 int main() {
@@ -48,22 +60,43 @@ int main() {
         return 1;
     }
 
-    
+    int choice;
 
-    // Ordenar registros por fecha y hora
-    sort(registros.begin(), registros.end(), compareRegistros);
+    cout << "Selecciona por que factor quiere ordenar (por su numero) :\n1) Fecha\n2) IP" << endl;
+    cin >> choice;
 
-    // Desplegar registros correspondientes a esas fechas
-    searchAndDisplay(registros);
+    switch(choice) {
+        case 1:
+            // Ordenar registros por fecha y hora
+            sort(registros.begin(), registros.end(), compareRegistrosDate);
 
-    // Almacenar en un archivo el resultado del ordenamiento
-    saveSortedData(registros);
+            // Desplegar registros correspondientes a esas fechas
+            searchAndDisplayDate(registros);
 
-    system("pause");
-    return 0;
+            // Almacenar en un archivo el resultado del ordenamiento
+            saveSortedData(registros);
+
+            system("pause");
+            break;
+
+        case 2:
+
+             // Ordenar registros por Ip
+            sort(registros.begin(), registros.end(), compareRegistrosIp);
+
+            // Desplegar registros correspondientes a estas Ip
+            searchAndDisplayIp(registros);
+
+            // Almacenar en un archivo el resultado del ordenamiento
+            saveSortedData(registros);
+
+            system("pause");
+            break;
+
+    }
 }
 
-void read(vector<Registro>& registros) {
+void readDate(vector<Registro>& registros) {
     ifstream archivo("bitacora.txt");
     if (!archivo.is_open()) {
         cout << "No se pudo leer el archivo" << endl;
@@ -82,7 +115,7 @@ void read(vector<Registro>& registros) {
     archivo.close();
 }
 
-void searchAndDisplay(const vector<Registro>& registros) {
+void searchAndDisplayDate(const vector<Registro>& registros) {
     // Solicitar al usuario las fechas de inicio y fin de búsqueda
     string startDate, endDate;
     cout << "Ingrese la fecha de inicio (MMM DD): ";
@@ -103,6 +136,25 @@ void searchAndDisplay(const vector<Registro>& registros) {
     }
 }
 
+void searchAndDisplayIp(const vector<Registro>& registros) {
+    // Solicitar al usuario las Ips de inicio y fin de búsqueda
+    string startIp, endIp;
+    cout << "Ingrese la IP de inicio : ";
+    cin >> startIp;
+    cout << "Ingrese la IP de fin : ";
+    cin >> endIp;
+
+    cout << "Registros entre " << startIp << " y " << endIp << ":" << endl;
+
+    for (const Registro& registro : registros){
+
+        string entryIp = registro.ip;
+
+        if ((entryIp >= startIp) && (entryIp <= endIp)) {         
+            cout << registro.fecha << " " << registro.dia << " " << registro.hora << " " << registro.ip << " " << registro.mensaje << endl;
+        }
+    }
+}
 
 void saveSortedData(const vector<Registro>& registros) {
     ofstream archivoOrdenado("bitacora_ordenada.txt");
@@ -112,7 +164,7 @@ void saveSortedData(const vector<Registro>& registros) {
     }
 
     for (const Registro& registro : registros) {
-        archivoOrdenado << registro.fecha << " " << registro.dia << " " << registro.hora << " " << registro.ip << " " << registro.mensaje << endl;
+        archivoOrdenado << registro.ip << " " << registro.fecha << " " << registro.dia << " " << registro.hora << " " << registro.mensaje << endl;
     }
 
     archivoOrdenado.close();
